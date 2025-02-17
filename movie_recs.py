9a0ba1436311c152a10ca91584dd9c8843d8dea6
dhruvilshah <200305124903@paruluniversity.ac.in>
2025-02-17 23:44:41 +0530

    first commit

---
+++ movie_recs.py
@@ -0,0 +1,48 @@
+import pymongo
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get values from environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+# Connect to MongoDB
+client = pymongo.MongoClient(MONGO_URI)
+db = client.sample_mflix
+collection = db.movies
+
+# Hugging Face API details
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+def generate_embedding(text: str) -> list[float]:
+    response = requests.post(
+        embedding_url,
+        headers={"Authorization": f"Bearer {HF_TOKEN}"},
+        json={"inputs": text}
+    )
+
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+    return response.json()
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+    {
+        "$vectorSearch": {
+            "queryVector": generate_embedding(query),
+            "path": "plot_embedding_hf",
+            "numCandidates": 100,
+            "limit": 4,
+            "index": "vector_index",
+        }
+    }
+])
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
---
+++ test.py
@@ -0,0 +1,23 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://corpweexalate:mSR6D8cjqLETQ2lQ@genai.35vaw.mongodb.net/?retryWrites=true&w=majority&appName=GenAI"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db=client.sample_mflix
+collection=db.movies
+
+items=collection.find().limit(5)
+
+for i in items:
+    print (i)
